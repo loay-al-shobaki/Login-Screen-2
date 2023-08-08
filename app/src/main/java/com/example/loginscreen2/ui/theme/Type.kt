4e4d9220d8e757f7adb45333ae9ab4da5cf0f162
Loay Al Shobaki <107764558+loay-al-shobaki@users.noreply.github.com>
2023-08-08 11:50:18 +0300

    add  fonts and images

--- app/src/main/java/com/example/loginscreen2/ui/theme/Type.kt
+++ app/src/main/java/com/example/loginscreen2/ui/theme/Type.kt
@@ -1,34 +1,59 @@
 package com.example.loginscreen2.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.loginscreen2.R
+
+
+val Inter = FontFamily(
+    listOf(
+        Font(resId = R.font.inter_medium, weight = FontWeight.Medium)
+    )
+)
+
+val Rubik = FontFamily(
+    listOf(
+        Font(resId = R.font.rubik_bold, weight = FontWeight.Bold)
+    )
+)
+
+val Robote = FontFamily(
+    listOf(
+        Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+        Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+        Font(resId = R.font.roboto_bold , weight = FontWeight.ExtraBold)
+    )
+)
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = Robote,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 32.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Robote,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Robote,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
+
+
 )
